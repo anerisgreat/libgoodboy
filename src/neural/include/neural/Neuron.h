3788ffa818365e777466f01bf3dba96b27bdc405
Aner Zakovitch <anerisgreat@gmail.com>
2018-11-25 07:27:05 -0800

    Added dependencies, still writing.

--- src/neural/include/neural/Neuron.h
+++ src/neural/include/neural/Neuron.h
@@ -1,15 +1,16 @@
 #include "libGoodBoyConfig.h"
 #include "neural/NeuralConfig.h"
 
+#include <vector>
+
 namespace PROJECT_NAMESPACE
 {
     virtual class Neuron
     {
         public:
-            Neuron(/*PREFILT_SHIFTREG, 
-                     FILTER, 
-                     POSTFILT_SHIFTREG, 
-                     ENDORPH_TAPS*/);
+            Neuron(
+                    
+                    unsigned int fitlers_len);
             ~Neuron();
 
             t_neural_val GetOutput();
@@ -17,6 +18,7 @@ namespace PROJECT_NAMESPACE
 
             void Endorphinize(t_neural_val amount);
             void ResetEndorphiniztionFlag();
+            t_neural_val CalcContribution();
 
             void BackProbe();
             void ForwardProbe();
@@ -28,33 +30,36 @@ namespace PROJECT_NAMESPACE
             
             void OnConnectedToOutput();
             void OnRemovedFromOutput();
-            //NEURON_ID_TYPE GetUUID();
+
+            boost::uuids:uuid GetUUID();
+
             //JSON GetJson();
             //STRING ToString();
         protected:
             virtual t_neural_val calcOutput() = 0;
             t_neural_val m_lastOutput;
 
+            //Consider why this exists
             virtual void postResetOutput() = 0;
             virtual void postResetProbe() = 0;
             virtual void endorphinizeSelf(t_neural_val amount) = 0;
             virtual void postFlush() = 0;
 
-            bool calcContribution();
         private:
+            void reset();
+
             bool m_checkedOutputFlag;
             bool m_forwardProbedFlag;
             bool m_backwardProbedFlag;
             bool m_endorphinizationFlag;
 
-            //NEURON_ID_TYPE UUID
-            
-            //VECTOR<NEURAL_CONNECTIONS> m_output_connections_vec 
+            boost::circular_buffer<t_neural_val> m_output_pre_filter_buffer;
+            boost::circular_buffer<t_neural_val> m_output_post_filter_buffer;
+            unsigned int m_filters_len;
+            t_neural_val* m_output_filter_taps;
+            t_neural_val* m_endorphinization_filter_taps;
 
-            //FIR_TAPS m_output_filter_taps;
-            //SHIFTREG m_output_shiftreg;
-
-            //FIR_TAPS m_endorphinization_taps;
-            //SHIFTREG m_output_history_shiftreg;
+            boost::uuids:uuid m_id;
+            vetor<NEURAL_CONNECTIONS> m_output_connections_vec 
     };
 }
--- src/neural/src/Neuron.cxx
+++ src/neural/src/Neuron.cxx
@@ -4,15 +4,30 @@
 
 namespace PROJECT_NAMESPACE
 {
-    Neuron::Neuron()
+    Neuron(t_neural_val* output_filter_taps
+            t_neural_val* endorphinization_filter_taps,
+            unsigned int filters_len)
+        :
+            m_checkedOutputFlag(false),
+            m_forwardProbedFlag(false),
+            m_backwardProbedFlag(false),
+            m_endorphinizationFlag(false),
+
+            m_output_pre_filter_buffer(filters_len),
+            m_output_post_filter_buffer(filters_len),
+
+            m_output_filter_len(filters_len),
+            m_output_filter_taps(outp_filt_taps),
+            m_endorphinization_filter_taps(endorphinization_filter_taps),
+
+            m_output_connections_vec(),
+
+            m_id(boost::uuids:random_generator()())
     {
-        m_checkedOutputFlag = false;
-        m_endorphinizationFlag = false;
     }
 
     Neuron::~Neuron()
     {
-
     }
 
     t_neural_val Neuron::GetOutput()
@@ -20,10 +35,18 @@ namespace PROJECT_NAMESPACE
         if(!m_checkedOutputFlag)
         {
             m_checkedOutputFlag = true;
-            m_lastOutput = calcOutput();
+            t_neural_val pre_filt_output = calcOutput();
+            m_output_pre_filter_buffer.push(pre_filt_output)
+            //TODO: Implement INNER
+            t_neural_val post_filt = inner(
+                    m_output_filter_taps,
+                    //TODO: head?!
+                    m_output_pre_filter_buffer.head(),
+                    m_output_filter_len);
+            m_output_post_filter_buffer.push(post_filt);
         }
-        //NEED TO FILTER HERE
-        return m_lastOutput;
+
+        return m_output_post_filter_buffer[0];
     }
 
     void Neuron::ResetOutputFlag()
@@ -43,5 +66,30 @@ namespace PROJECT_NAMESPACE
     void Neuron::ResetEndorphinizationFlag()
     {
         m_endorphinizationFlag = false;
+    }
+
+    t_neural_val Neuron::CalcContribution()
+    {
+        return inner(
+                m_output_post_filter_buffer.head(),
+                m_endorphinization_filter_taps,
+                m_filters_len);
+    }
+
+    void Neuron::BackProbe()
+    {
+        if(!m_backwardProbedFlag)
+        {
+            m_backwardProbedFlag = true;
+            postkkkkkkkkk
+        }
+    }
+
+    void Neuron::reset()
+    {
+        m_checkedOutputFlag = false;
+        m_endorphinizationFlag = false;
+
+        //TODO: IMPLEMENT AND RENAME
     }
 }
